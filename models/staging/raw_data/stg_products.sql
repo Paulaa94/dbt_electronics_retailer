9d3f5806b9e4e61f32c42bbb485adbe6a39e5eee
paula.rakowska8@gmail.com <paula.rakowska8@gmail.com>
2025-06-17 08:43:49 +0000

    stg_products.sql created

--- models/staging/raw_data/stg_products.sql
+++ models/staging/raw_data/stg_products.sql
@@ -1,25 +1,20 @@
 with source as (
-
     select * from {{ source('raw', 'Products') }}
-
 ),
 
 cleaned as (
-
     select
-        cast("ProductKey" as int) as product_key,
-        trim("Product Name") as product_name,
-        trim("Brand") as brand,
-        trim("Color") as color,
-        cast(replace("Unit Cost USD", '$', '') as float64) as unit_cost_usd,
-        cast(replace("Unit Price USD", '$', '') as float64) as unit_price_usd,
-        cast("SubcategoryKey" as string) as subcategory_key,
-        trim("Subcategory") as subcategory,
-        cast("CategoryKey" as string) as category_key,
-        trim("Category") as category
-
+        cast(ProductKey as int64) as product_key,
+        trim(`Product Name`) as product_name,
+        trim(Brand) as brand,
+        trim(Color) as color,
+        cast(`Unit Cost USD` as float64) as unit_cost_usd,       
+        cast(`Unit Price USD` as float64) as unit_price_usd,     
+        cast(SubcategoryKey as string) as subcategory_key,
+        trim(Subcategory) as subcategory,
+        cast(CategoryKey as string) as category_key,
+        trim(Category) as category
     from source
-
 )
 
 select * from cleaned
